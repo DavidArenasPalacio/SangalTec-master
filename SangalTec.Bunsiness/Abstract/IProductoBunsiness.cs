829cab91cc6d40d4249373db222a1fd250d4c558
David <davidarenasp123@gmail.com>
2021-09-16 21:06:41 -0500

    Función crear para productos y categorias

---
+++ SangalTec.Bunsiness/Abstract/IProductoBunsiness.cs
@@ -0,0 +1,18 @@
+﻿using SangalTec.Models.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SangalTec.Bunsiness.Abstract
+{
+    public interface IProductoBunsiness
+    {
+        Task<IEnumerable<Producto>> ObtenerProductos();
+
+        void Crear(Producto producto);
+
+        Task<bool> GuardarCambios();
+    }
+}
